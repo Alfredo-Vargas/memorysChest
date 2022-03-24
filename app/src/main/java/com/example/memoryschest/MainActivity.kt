1f7f54997a2a0b451a73964b1ad5ccd6f0491404
Alfredo <alfredo.vargas-paredes@outlook.com>
2022-03-24 23:33:42 +0100

    MemorysChest repo started

---
+++ app/src/main/java/com/example/memoryschest/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.memoryschest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// <a href="https://www.vecteezy.com/free-vector/glossy-button">Glossy Button Vectors by Vecteezy</a>
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
