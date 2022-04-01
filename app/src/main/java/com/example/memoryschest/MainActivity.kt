a637dc867f682e035597321c22970efdac365fa0
Alfredo <alfredo.vargas-paredes@outlook.com>
2022-04-01 16:04:27 +0200

    2022-04-01 16:0427

--- app/src/main/java/com/example/memoryschest/MainActivity.kt
+++ app/src/main/java/com/example/memoryschest/MainActivity.kt
@@ -12,6 +12,7 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
     // normal starting main activity
     override fun onCreate(savedInstanceState: Bundle?) {
+        val themeC: String = ""
         super.onCreate(savedInstanceState)
         // load the preferences related to the app name
         //val preferences: SharedPreferences = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE)
@@ -51,4 +52,66 @@ class MainActivity : AppCompatActivity() {
             spinner.adapter = adapter
         }
     }
+
+    private fun saveProfile() {
+        // we load the string from resources which initially has an empty string ""
+        val themeChosen: String = R.string.chosen_theme_value.toString()
+        // now create an instance of SharedPreferences whose value is given by the previous one
+        val themePreference: SharedPreferences = getSharedPreferences("chosenTheme", Context.MODE_PRIVATE)
+        // we create an editor instance to be able to modify the preferences
+        val themePreferenceEditor: SharedPreferences.Editor = themePreference.edit()
+        themePreferenceEditor.apply(){
+            putString("THEME_KEY", themeChosen)     // save the dictionary key-pair
+            putBoolean("SAVED_THEME", !themeChosen.isNullOrBlank())
+        }.apply()
+        // here to insert the Data saved
+    }
+
+    private fun loadProfile() {
+        // we load the chosen theme preference of the user
+        val themePreference: SharedPreferences = getSharedPreferences("chosenTheme", Context.MODE_PRIVATE)
+        val themeChosen: String? = themePreference.getString("THEME_KEY", null)     // default value null
+        val themeSaved: Boolean = themePreference.getBoolean("SAVED_THEME", false)  // default value false
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
